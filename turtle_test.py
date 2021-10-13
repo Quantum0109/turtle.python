fc8cfcb247682ade1a17dea37c6eec6199880d4c
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2021-10-13 17:11:43 +0300

    Add files via upload

--- turtle_test.py
+++ turtle_test.py
@@ -2,28 +2,30 @@ import turtle as t
 
 """
     TODO: узнать размер экрана пользователя
-        Узнать корды
+          узнать корды
 """
 
-def draw_square(side_lenght):
-    for i in range (4):
+def draw_square(side_lenght = 30, x = -100, y = -100, line_color = "#FF3300", pen_size = 1):
+    # setup
+    t.penup()
+    t.goto(x, y)
+    t.pencolor(line_color)
+    t.pensize(pen_size)
+    t.pendown()
+
+    for number in range (4):
         t.forward(side_lenght)
         t.left(90)
+    t.setheading(0)
 
-# setup
+# general setup
 t.shape("turtle")
 t.speed(1)
-t.pencolor("#00FF00")
-t.pensize(2)
-t.goto(0, 0)
-t.setheading(0)
-
-#выполнение
-draw_square(20)
-draw_square(200)
-draw_square(400)
-draw_square(600)
 
 
-#ждём выхода
+# выполнение
+draw_square(line_color = "#FFFF00", x = 150, side_lenght = 100, pen_size = 10)
+
+
+# ждём выхода
 t.done()
